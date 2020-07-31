df7168ba4c28d27868524100f4da0c818f35d160
Adam Crampton <adam@adamcrampton.com>
2020-08-01 08:29:22 +1000

    Set up AdminLTE

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,107 +1,48 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('adminlte::page')
 
-<head>
+@section('title', 'Dashboard')
 
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
 
-  <title>Simple Sidebar - Start Bootstrap Template</title>
+@section('content')
+<table class="table table-striped table-hover">
+    <thead>
+        <th class="text-center">Number</th>
+        <th>Name</th>
+        <th>Colour</th>
+        <th class="text-center">Year</th>
+        <th>Type</th>
+        <th>Series</th>
+        <th class="text-center">Series Number</th>
+        <th class="text-center">Treasure Hunt</th>
+        <th class="text-center">Super Treasure Hunt</th>
+        <th class="text-center">Owned</th>
+    </thead>
+    <tbody>
+        @foreach ($cars as $car)
+            <tr>
+                <td class="text-center">{{ $car->number }}</td>
+                <td>{{ $car->name }}</td>
+                <td>{{ $car->colour }}</td>
+                <td class="text-center">{{ $car->year }}</td>
+                <td>{{ $car->type->name }}</td>
+                <td>{{ $car->series->name }}</td>
+                <td class="text-center">{{ $car->series_number }}</td>
+                <td class="text-center">{!! $car->treasure_hunt ? '<i class="fa fa-check text-success"></i>' : '<i class="fa fa-times text-danger"></i>' !!}</td>
+                <td class="text-center">{!! $car->super_treasure_hunt ? '<i class="fa fa-check text-success"></i>' : '<i class="fa fa-times text-danger"></i>' !!}</td>
+                <td class="text-center">{!! $car->owned ? '<i class="fa fa-check text-success"></i>' : '<i class="fa fa-times text-danger"></i>' !!}</td>
+            </tr>    
+        @endforeach
+    </tbody>
+</table>
+@stop
 
-  <!-- Bootstrap core CSS -->
-  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="css/font-awesome.min.css" rel="stylesheet">
+@section('css')
+    <link rel="stylesheet" href="/css/dashboard.css">
+@stop
 
-  <!-- Custom styles for this template -->
-  <link href="css/simple-sidebar.css" rel="stylesheet">
-
-</head>
-
-<body>
-
-  <div class="d-flex" id="wrapper">
-
-    <!-- Sidebar -->
-    <div class="bg-light border-right" id="sidebar-wrapper">
-      <div class="sidebar-heading">Hot Wheels List </div>
-      <div class="list-group list-group-flush">
-        <a href="/" class="list-group-item list-group-item-action bg-light">Home</a>
-      </div>
-    </div>
-    <!-- /#sidebar-wrapper -->
-
-    <!-- Page Content -->
-    <div id="page-content-wrapper">
-
-      <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
-        <button class="btn btn-primary" id="menu-toggle">Toggle Menu</button>
-
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
-            <li class="nav-item active">
-              <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-
-      <div class="container-fluid">
-        <h1 class="mt-4">Hot Wheels Collection</h1>
-            <table class="table table-striped table-hover">
-                <thead>
-                    <th class="text-center">Number</th>
-                    <th>Name</th>
-                    <th>Colour</th>
-                    <th class="text-center">Year</th>
-                    <th>Type</th>
-                    <th>Series</th>
-                    <th class="text-center">Series Number</th>
-                    <th class="text-center">Treasure Hunt</th>
-                    <th class="text-center">Super Treasure Hunt</th>
-                    <th class="text-center">Owned</th>
-                </thead>
-                <tbody>
-                    @foreach ($cars as $car)
-                        <tr>
-                            <td class="text-center">{{ $car->number }}</td>
-                            <td>{{ $car->name }}</td>
-                            <td>{{ $car->colour }}</td>
-                            <td class="text-center">{{ $car->year }}</td>
-                            <td>{{ $car->type->name }}</td>
-                            <td>{{ $car->series->name }}</td>
-                            <td class="text-center">{{ $car->series_number }}</td>
-                            <td class="text-center">{!! $car->treasure_hunt ? '<i class="fa fa-check text-success"></i>' : '<i class="fa fa-times text-danger"></i>' !!}</td>
-                            <td class="text-center">{!! $car->super_treasure_hunt ? '<i class="fa fa-check text-success"></i>' : '<i class="fa fa-times text-danger"></i>' !!}</td>
-                            <td class="text-center">{!! $car->owned ? '<i class="fa fa-check text-success"></i>' : '<i class="fa fa-times text-danger"></i>' !!}</td>
-                        </tr>    
-                    @endforeach
-                </tbody>
-            </table>
-      </div>
-    </div>
-    <!-- /#page-content-wrapper -->
-
-  </div>
-  <!-- /#wrapper -->
-
-  <!-- Bootstrap core JavaScript -->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-  <!-- Menu Toggle Script -->
-  <script>
-    $("#menu-toggle").click(function(e) {
-      e.preventDefault();
-      $("#wrapper").toggleClass("toggled");
-    });
-  </script>
-
-</body>
-
-</html>
+@section('js')
+    <script src="/js/dashboard.js"></script>
+@stop
